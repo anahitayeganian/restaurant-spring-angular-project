c3c822159ecf4af956ecc7eb6b001de44307c13d
anahitayeganian <anahitayeganian@gmail.com>
2024-03-13 20:40:28 +0100

    URLs for the resource category have been changed

--- src/main/java/com/ay/restaurant/rest/CategoryRest.java
+++ src/main/java/com/ay/restaurant/rest/CategoryRest.java
@@ -7,13 +7,13 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Map;
 
-@RequestMapping(path = "/category")
+@RequestMapping(path = "/categories")
 public interface CategoryRest {
 
     @PostMapping(path = "/add")
     public ResponseEntity<String> addNewCategory(@RequestBody(required = true) Map<String,String> requestMap);
 
-    @GetMapping(path = "/get")
+    @GetMapping(path = "/all")
     public ResponseEntity<List<Category>> getAllCategories(@RequestParam(required = false) String filterList);
 
     @PostMapping(path ="/update")
