a493ff653161e09e256292edae1ccea2541881e5
anahitayeganian <anahitayeganian@gmail.com>
2024-03-21 18:30:31 +0100

    Implement frontend functions for the login page

---
+++ restaurant-frontend-angular/src/app/components/login/login.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+
+  loginForm!: FormGroup;
+  isSubmitted = false;
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    })
+  }
+
+  get fc() {
+    return this.loginForm.controls;
+  }
+
+  submit() {
+    this.isSubmitted = true;
+    if(this.loginForm.invalid)
+      return;
+    //alert(`email: ${this.loginForm.get('email')?.value}, password: ${this.loginForm.get('password')?.value}`);
+    alert(`email: ${this.fc['email'].value}, password: ${this.fc['password'].value}`);
+  }
+
+}
