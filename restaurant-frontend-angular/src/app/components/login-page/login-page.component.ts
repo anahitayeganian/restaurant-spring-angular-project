1bc39f664b2446312734da8ebb65c5eaff36e5cb
anahitayeganian <anahitayeganian@gmail.com>
2024-03-23 12:16:10 +0100

    Modified the login page component by replacing the previous code with text-input and default-button components

--- restaurant-frontend-angular/src/app/components/login-page/login-page.component.ts
+++ restaurant-frontend-angular/src/app/components/login-page/login-page.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormGroup, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login-page',
@@ -16,8 +16,36 @@ export class LoginPageComponent implements OnInit {
   ngOnInit(): void {
     this.loginForm = this.formBuilder.group({
       email: ['', [Validators.required, Validators.email]],
-      password: ['', Validators.required]
-    })
+      password: ['', [Validators.required, this.validatePassword()]]
+    });
+  }
+
+  /* Custom validator function for password */
+  validatePassword(): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      const value = control.value as string;
+      const errors: ValidationErrors = {};
+      if (!value) {
+        return null;
+      }
+      if (!/[A-Z]/.test(value)) {
+        errors['uppercaseRequired'] = true;
+        return errors;
+      }
+      if (!/\d/.test(value)) {
+        errors['numberRequired'] = true;
+        return errors;
+      }
+      if (!/[!*,.?]/.test(value)) {
+        errors['specialCharRequired'] = true;
+        return errors;
+      }
+      if (value.length < 8) {
+        errors['minLengthRequired'] = true;
+        return errors;
+      }
+      return null; // Password meets all criteria
+    };
   }
 
   get fc() {
@@ -26,7 +54,7 @@ export class LoginPageComponent implements OnInit {
 
   submit() {
     this.isSubmitted = true;
-    if(this.loginForm.invalid)
+    if (this.loginForm.invalid)
       return;
     //alert(`email: ${this.loginForm.get('email')?.value}, password: ${this.loginForm.get('password')?.value}`);
     alert(`email: ${this.fc['email'].value}, password: ${this.fc['password'].value}`);
